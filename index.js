bfda80afbe2e7ff1f5fc13dad71e17958e87c3aa
Alex Gathungu <alex.gathungu@student.moringaschool.com>
2022-08-23 05:20:39 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function addingEventListener() {
+ input = document.getElementById('input');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});input = document.getElementById('input');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+input.addEventListener('click', clickAlert);
 }
